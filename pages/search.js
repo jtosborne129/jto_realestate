60af9462cc5f1fbad68f384e9c200aa57e69c4dd
Joseph Osborne <jtosborne129@gmail.com>
2023-02-24 12:21:08 -0500

    Double check imports and check to make sure build works

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import { BsFilter } from "react-icons/bs";
 
 import Property from "../components/Property";
 import SearchFilters from "../components/SearchFilters";
-import { baseURL, fetchApi } from "../utils/fetchAPI";
+import { baseURL, fetchApi } from '../utils/fetchAPI';
 import noresult from "../assets/images/noresult.svg";
 
 const Search = ({ properties }) => {
